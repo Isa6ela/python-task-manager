9eaa5416e6494848948e27a67385712f37584d9c
Iza Godyn <izabela0411x@gmail.com>
2026-09-03 16:59:41 +0200

    Add input validation and error handling

--- main.py
+++ main.py
@@ -10,21 +10,30 @@ def menu():
     5. Exit
     """)
 
-#TASKS LISTS
+# TASKS LIST
 tasks_list = {}
 
 # MENU CHOICE
 while True:
     menu()
 
-    choice = int(input("Enter your choice: "))
-    #ADD TASK
+    # ERROR HANDLING
+    try:
+        choice = int(input("Enter your choice: "))
+    except ValueError:
+        print("Invalid choice. Try again. \n" )
+        continue
+
+    # ADD TASK
     if choice == 1:
         while True:
             task = input("Enter your task: ")
             print("press q to exit")
-            if task == "q":
+            if task in ["q", "Q"]:
                 break
+            if task == "":
+                print("Invalid task. Try again. \n" )
+                continue
             number = len(tasks_list)+1
 
             task_data = {
@@ -35,7 +44,7 @@ while True:
             tasks_list[number] = task_data
 
 
-    #SHOW TASKS
+    # SHOW TASKS
     elif choice == 2:
         for num, task in tasks_list.items():
             if task["completed"]:
@@ -44,21 +53,30 @@ while True:
                 print(f"{num}. [ ] {task['task']}")
 
 
-    #COMPLETE TASK
+    # COMPLETE TASK
     elif choice == 3:
             while True:
-                completed_task = input("Enter task number to complete: ")
+                completed_task = input("Enter number of task to complete: ")
                 print("press q to exit")
-                if completed_task == "q":
+                if completed_task in ["q", "Q"]:
                     break
-                completed_task = int(completed_task)
-                if completed_task in tasks_list:
-                    tasks_list[completed_task]["completed"] = True
+                if completed_task.isdigit():
+                    completed_task = int(completed_task)
+                    if completed_task in tasks_list:
+                        tasks_list[completed_task]["completed"] = True
+                    else:
+                        print("Invalid number. Try again. \n" )
+                else:
+                    print("Invalid number. Try again. \n" )
 
 
-    #DELETE TASK
+    # DELETE TASK
     elif choice == 4:
-        delete_task = int(input("Enter number of task to delete: "))
+        try:
+            delete_task = int(input("Enter number of task to delete: "))
+        except ValueError:
+            print("Invalid number. Try again. \n" )
+            continue
         if delete_task in tasks_list:
             del tasks_list[delete_task]
 
@@ -70,9 +88,17 @@ while True:
 
             print("Task deleted")
 
+        else:
+            print("Invalid number. Try again. \n" )
+
+
+
 
     # EXIT THE PROGRAM
     elif choice == 5:
         print("=== EXITING ===")
         break
 
+
+    else:
+        print("Invalid choice. Try again. \n" )
